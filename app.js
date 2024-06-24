1233247f3109e98e2a20c4b2a9fc2489507dbbaa
Rich Boyce <122512687+rboyce212@users.noreply.github.com>
2024-06-24 10:51:05 -0600

    beginning database

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-const express = require("express");// Dependency // requiring express from our node modules
-const app = express(); // configuration // storing an instance of express in our app variable 
-const animesController = require("./controllers/animes")
+const express = require("express"); // Dependency // requiring express from our node modules
+const app = express(); // configuration // storing an instance of express in our app variable
+const animesController = require("./controllers/animes");
 
+app.use(express.json()); // parse incoming json - when we make an http request we receive a json string and we parse it into a json object. We need to place our json parser before our routes
 
-app.use(express.json()) // parse incoming json - when we make an http request we receive a json string and we parse it into a json object. We need to place our json parser before our routes 
+app.use("/animes", animesController); // binding our path to the routes found in our controller file
 
-app.use("/animes", animesController); // binding our path to the routes found in our controller file 
-
-
-app.get("/", (_, response)=>{ // route for our "homepage"
-    response.send("Welcome to Animania")
-})
+app.get("/", (_, response) => {
+  // route for our "homepage"
+  response.send("Welcome to Animania");
+});
 
 // catch all for request that did not match any route(method & paths)
-app.get("*",(_, res)=>{
-    res.status(404).send("The request you are looking for doesn't exist.")
-} )
+app.get("*", (_, res) => {
+  res.status(404).send("The request you're looking for doesn't exist.");
+});
 
 module.exports = app;
